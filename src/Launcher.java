927a40d21da26125b22cc009bb85ad031ad791ef
StrawberryMilk05 <tobias.schaelte@outlook.de>
2021-10-04 15:24:12 +0200

    init commit

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,7 +1,7 @@
 import core.ScreenSize;
 import menu.Menu;
 public class Launcher {
-
+//test
     public static void main(String[] args) {
 
         new Menu(ScreenSize.getWidth(), ScreenSize.getHeight());
