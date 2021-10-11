c0dab830716d737457f3a72d48715eeba3a99a5f
StrawberryMilk05 <tobias.schaelte@outlook.de>
2021-10-11 11:45:51 +0200

    init commit

--- src/game/state/GameState.java
+++ src/game/state/GameState.java
@@ -1,7 +1,6 @@
 package game.state;
 
 import controller.PlayerController;
-import entity.Grass;
 import entity.Player;
 import input.Input;
 
@@ -11,10 +10,10 @@ public class GameState extends State {
     public GameState(Input input) {
         super(input);
 
-        Grass grass = new Grass(64, 64, "grass");
+        //Grass grass = new Grass(64, 64, "grass");
         Player player = new Player(new PlayerController(input), spriteLibrary);
 
-        gameObjects.add(grass);
+        //gameObjects.add(grass);
         gameObjects.add(player);
     }
 }
