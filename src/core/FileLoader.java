3cdc6a85530d5958283a8354de076ff0f51617a1
StraberryMilk05 <tobias.schaelte@outlook.de>
2021-11-03 09:42:20 +0100

    init commit

--- src/core/FileLoader.java
+++ src/core/FileLoader.java
@@ -8,7 +8,7 @@ public class FileLoader {
 
     public static void save(String data, String filename){
         try {
-            BufferedWriter writer = new BufferedWriter(new FileWriter(filename));
+            BufferedWriter writer = new BufferedWriter(new FileWriter("resources/" + filename));
             writer.write(data);
             writer.close();
         } catch (IOException e) {
@@ -19,14 +19,14 @@ public class FileLoader {
     public static String load(String filename){
         String input = null;
         
-        File file = new File(filename);
+        File file = new File("resources/" + filename);
         if(!file.exists()){
             System.err.println("ERROR! file '" + filename + "' not found");
             return null;
         }
         
         try {
-            input = new String(Files.readAllBytes(Path.of(filename)));
+            input = new String(Files.readAllBytes(Path.of("resources/" + filename)));
         } catch (IOException e) {
             System.err.println("ERROR! failed to load file '" + filename + "'");
             return null;
