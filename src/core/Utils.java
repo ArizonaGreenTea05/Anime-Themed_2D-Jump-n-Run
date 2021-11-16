e8d987e60d01299f64f7f23f010c5ac63ac48a09
StrawberryMilk05 <tobias.schaelte@outlook.de>
2021-11-16 18:18:10 +0100

    init commit

---
+++ src/core/Utils.java
@@ -0,0 +1,11 @@
+package core;
+
+public class Utils {
+    public static void sleep(long millis){
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
