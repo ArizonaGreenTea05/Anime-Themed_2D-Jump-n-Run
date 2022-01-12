4e1553fc6ba7fd18bf004ca563037743156d9ded
StrawberryMilk05 <tobias.schaelte@outlook.de>
2022-01-12 13:58:18 +0100

    init commit

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,7 +1,4 @@
 import menu.Menu;
-
-    public class Launcher {
-    public static void main(String[] args) {
+public class Launcher {public static void main(String[] args) {
         new Menu();
-    }
-}
+    }}
