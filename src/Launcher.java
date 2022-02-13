5de0523ea2a4eae7a61bf712cf0f5abe372d2c87
StrawberryMilk05 <tobias.schaelte@outlook.de>
2022-02-13 18:30:22 +0100

    V 1.3

--- src/Launcher.java
+++ src/Launcher.java
@@ -3,6 +3,6 @@ public class Launcher {
     // Launcher-class -> launches the menu
     public static void main(String[] args) {
         // defining game-version (which is shown in title)
-        new Menu("V 1.2 - BETA");
+        new Menu("V 1.3 - Facharbeit Pre-Release");
     }
 }
