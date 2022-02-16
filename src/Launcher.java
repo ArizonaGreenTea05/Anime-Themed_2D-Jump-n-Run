cb349c36a6294fbe65866ecb3e3c37a7965f2b4a
StrawberryMilk05 <tobias.schaelte@outlook.de>
2022-02-16 11:13:02 +0100

    renamed project, prints needed jdk, TODO added

--- src/Launcher.java
+++ src/Launcher.java
@@ -2,6 +2,10 @@ import menu.Menu;
 public class Launcher {
     // Launcher-class -> launches the menu
     public static void main(String[] args) {
+
+        // TODO: find out which java version is needed and add to print-output
+        System.err.println("Oracle OpenJDK version 17.0.1 needed");
+
         // defining game-version (which is shown in title)
         new Menu("V 1.3.1 - Facharbeit Pre-Release");
     }
