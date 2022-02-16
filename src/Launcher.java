bcf4c5e2164de4f51f86559255aafdf570a58c63
StrawberryMilk05 <tobias.schaelte@outlook.de>
2022-02-16 21:45:58 +0100

    updated to newest java-version

--- src/Launcher.java
+++ src/Launcher.java
@@ -3,8 +3,7 @@ public class Launcher {
     // Launcher-class -> launches the menu
     public static void main(String[] args) {
 
-        // TODO: find out which java version is needed and add to print-output
-        System.err.println("Oracle OpenJDK version 17.0.2 needed");
+        System.err.println("requirements: \n  JRE 1.8.0_321-b07 \n  JDK 17.0.2");
 
         // defining game-version (which is shown in title)
         new Menu("V 1.3.1 - Facharbeit Pre-Release");
